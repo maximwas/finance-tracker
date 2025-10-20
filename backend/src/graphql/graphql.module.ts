402bea7a92ef5a272865061ee015f8764c9e25cc
m.vasianin <m.vasianin@viseven.com>
2025-10-20 11:09:55 +0300

    [maximwas]: add transaction module

--- backend/src/graphql/graphql.module.ts
+++ backend/src/graphql/graphql.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from 'src/api/modules/auth/auth.module';
 
 import { CategoryModule } from './modules/category/category.module';
 import { UserModule } from './modules/user/user.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -16,6 +17,7 @@ import { UserModule } from './modules/user/user.module';
     AuthModule,
     UserModule,
     CategoryModule,
+    TransactionModule,
   ],
   exports: [UserModule, CategoryModule],
 })
