eb544444d815419898ac216fcd9a509faec6732a
m.vasianin <m.vasianin@viseven.com>
2025-11-20 17:30:43 +0200

    [maximwas]: update sidebar

--- frontend/src/components/layout/app-sidebar.tsx
+++ frontend/src/components/layout/app-sidebar.tsx
@@ -1,8 +1,16 @@
-import { BarChart3, LayoutDashboard, Receipt, Settings, Sparkles, Tag } from 'lucide-react';
+'use client';
+
+import { Dialog, DialogContent, DialogTrigger } from '@radix-ui/react-dialog';
+import { BarChart3, LayoutDashboard, LogOut, Receipt, Settings, Sparkles, Tag } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import * as React from 'react';
 
+import { cn } from '@/lib/utils';
+
 import { Logo } from '../logo/logo';
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import { Button } from '../ui/button';
 import {
   Sidebar,
   SidebarContent,
@@ -64,28 +72,61 @@ const SIDEBAR_MENU: ISidebarMenu[] = [
 ];
 
 export function AppSidebar(): React.JSX.Element {
+  const pathname = usePathname();
+
+  const isActive = (url: string): boolean => {
+    return url === pathname;
+  };
+
   return (
-    <Sidebar className="">
-      <SidebarHeader>
-        <div className="flex gap-2 px-3 py-4">
-          <Logo width={25} height={25}></Logo>
-          <div className="flex flex-col justify-center gap-1">
-            <TypographyH5 className="text-base bg-linear-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text text-transparent flex gap-2 items-center">
-              FinTracker
-              <Sparkles className="w-5 h-5 text-purple-500" />
-            </TypographyH5>
-            <TypographyP className="text-xs text-muted-foreground">Smart Finance</TypographyP>
-          </div>
+    <Sidebar>
+      <SidebarHeader className="flex-row gap-2 p-4">
+        <Logo width={25} height={25}></Logo>
+        <div className="flex flex-col justify-center gap-1">
+          <TypographyH5 className="text-base bg-linear-to-r from-indigo-600 via-purple-600 to-pink-600 bg-clip-text text-transparent flex gap-2 items-center">
+            FinTracker
+            <Sparkles className="w-5 h-5 text-purple-500" />
+          </TypographyH5>
+          <TypographyP className="text-xs text-muted-foreground">Smart Finance</TypographyP>
         </div>
       </SidebarHeader>
+
       <SidebarSeparator />
+
       <SidebarContent>
-        <SidebarGroup>
+        <SidebarGroup className="p-4">
+          <div className="flex items-center gap-3 p-3 rounded-xl bg-linear-to-br from-background/50 to-muted/50 hover:from-accent/50 hover:to-accent transition-all duration-300 border border-border/50">
+            <div className="relative">
+              <div className="absolute inset-0 bg-linear-to-br from-blue-500 to-purple-500 rounded-full blur-md opacity-50"></div>
+              <Avatar className="h-12 w-12 border-2 border-white/50 dark:border-gray-700/50 relative">
+                <AvatarImage src="" alt="IП" />
+                <AvatarFallback className="bg-linear-to-br from-blue-500 to-purple-500 text-white">
+                  IП
+                </AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="flex-1 min-w-0">
+              <p className="text-sm truncate text-foreground">Іван Петренко</p>
+            </div>
+          </div>
+        </SidebarGroup>
+
+        <SidebarSeparator />
+
+        <SidebarGroup className="p-4">
           <SidebarGroupContent>
-            <SidebarMenu>
+            <SidebarMenu className="gap-5">
               {SIDEBAR_MENU.map((item) => (
                 <SidebarMenuItem key={item.id}>
-                  <SidebarMenuButton asChild>
+                  {isActive(item.url) ? (
+                    <div
+                      className={cn(
+                        'absolute inset-0 bg-linear-to-r from-pink-500 to-rose-500 opacity-100 rounded-xl',
+                        item.gradient,
+                      )}
+                    ></div>
+                  ) : null}
+                  <SidebarMenuButton variant={isActive(item.url) ? 'active' : 'default'} asChild>
                     <Link href={item.url}>
                       <item.icon />
                       <span>{item.label}</span>
@@ -96,10 +137,24 @@ export function AppSidebar(): React.JSX.Element {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
-        <SidebarSeparator />
-        <SidebarGroup></SidebarGroup>
       </SidebarContent>
-      <SidebarFooter />
+
+      <SidebarSeparator />
+
+      <SidebarFooter className="p-4">
+        <Dialog>
+          <DialogTrigger>
+            <Button
+              variant="ghost"
+              className="w-full justify-start gap-3 text-red-600 hover:text-red-700 hover:bg-red-50 dark:hover:bg-red-950/20 group"
+            >
+              <LogOut className="w-5 h-5 group-hover:-translate-x-1 transition-transform duration-300" />
+              <span>Logout</span>
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]"></DialogContent>
+        </Dialog>
+      </SidebarFooter>
     </Sidebar>
   );
 }
